4129ae306c6bc5ae2e65cab64a34a1c6cfced559
Christopher Svec <csvec@irobot.com>
2018-02-12 21:15:11 -0500

    Lane curve + lines drawn using Udacity suggestions.

--- project.py
+++ project.py
@@ -454,7 +454,7 @@ def find_lane_lines(orig_image, top_down_binary_image):
     leftx_base = np.argmax(histogram[:midpoint])
     rightx_base = np.argmax(histogram[midpoint:]) + midpoint
 
-    if 1:
+    if 0:
         g_subplotter.setup(cols=1,rows=2)
         g_subplotter.next(top_down_binary_image, 'bin')
         g_subplotter.next(histogram, 'hist', just_plot=True)
@@ -526,7 +526,7 @@ def find_lane_lines(orig_image, top_down_binary_image):
     left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
     right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
 
-    if 0:
+    if 1:
         g_subplotter.setup(cols=2,rows=2)
         g_subplotter.next(orig_image, 'orig')
         g_subplotter.next(top_down_binary_image, 'top_down')
@@ -540,6 +540,38 @@ def find_lane_lines(orig_image, top_down_binary_image):
         plt.xlim(0, 1280)
         plt.ylim(720, 0)
         #plt.show()
+
+    if 1: 
+        # Create an image to draw on and an image to show the selection window
+        out_img = np.dstack((top_down_binary_image, top_down_binary_image, top_down_binary_image))*255
+        window_img = np.zeros_like(out_img)
+        # Color in left and right line pixels
+        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+        
+        # Generate a polygon to illustrate the search window area
+        # And recast the x and y points into usable format for cv2.fillPoly()
+        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+        left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, 
+                                      ploty])))])
+        left_line_pts = np.hstack((left_line_window1, left_line_window2))
+        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+        right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, 
+                                      ploty])))])
+        right_line_pts = np.hstack((right_line_window1, right_line_window2))
+        
+        # Draw the lane onto the warped blank image
+        cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
+        cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
+        result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+        #plt.imshow(result)
+        g_subplotter.next(result, 'result')
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)
+
+    if 1:
         g_subplotter.show()
 
 def process_image(image_filename):
